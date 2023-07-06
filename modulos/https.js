83a940be91c22c1deafa9829da4afce686ff1ade
pabongamas <pabongamas@gmail.com>
2023-07-06 18:01:49 -0500

    commit http

---
+++ modulos/https.js
@@ -0,0 +1,40 @@
+const http=require('http');
+
+
+// http.createServer(function(req,res){
+//     console.log('Nueva peticion !');
+//     console.log(req.url);
+
+//     res.writeHead(201,{'Content-Type':'text/plain'})
+
+//     //ESCRIBIR RESPUESTA AL USUARIO
+//     res.write('Hola , ya se usar HTTP DE NODEJS');
+//     res.end();
+// }).listen(3000);
+
+http.createServer(router).listen(3000);
+
+function router(req,res){
+    console.log('Nueva peticion !');
+        console.log(req.url);
+        
+        switch(req.url){
+            case '/hola':
+                res.write('Hola que tal');
+                res.end();
+                break;
+            default:
+                res.write("Error 404 no se lo que quieres");
+                res.end();
+        }
+
+        res.writeHead(201,{'Content-Type':'text/plain'})
+    
+        //ESCRIBIR RESPUESTA AL USUARIO
+        res.write('Hola , ya se usar HTTP DE NODEJS');
+        res.end();
+}
+
+
+
+console.log("Escuchando http en el puerto 3000");
